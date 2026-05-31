865e22b53e3404880fd5e8a9c5465957565597ca
ACalebN <Adam@nettleship-group.com>
2026-05-31 13:46:39 +0100

    have 2 questions left

--- index.js
+++ index.js
@@ -2,3 +2,40 @@
 /**
  * Add your functions here...
  */
+
+function targetTextToConsole(event) {
+    let text = event.target.textContent;
+    console.log(text);
+}
+
+function tttcAttacher() {
+    button0.addEventListener('click', targetTextToConsole);
+}
+
+function lovelyParaAttacher() {
+    thisisalovelyparagraph.addEventListener('click', lovelyToggle)
+}
+
+function lovelyButtonAttacher() {
+    button1.addEventListener('click', lovelyToggle)
+}
+
+function concatHandler() {
+    const in1Value = in1.value;
+    const in2Value = in2.value;
+    out1.textContent = in1Value + in2Value;
+}
+
+function concatAttacher() {
+    in1.addEventListener('change', concatHandler);
+    in2.addEventListener('change', concatHandler);
+}
+
+function snitchAttacher() {
+    mousewatcher.addEventListener('mouseover', snitchUpdater)
+    mousewatcher.addEventListener('mouseout', snitchUpdater)
+}
+
+function reportAttacher() {
+    
+}
